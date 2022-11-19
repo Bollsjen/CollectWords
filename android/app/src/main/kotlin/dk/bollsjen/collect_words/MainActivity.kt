cd3c7132942a85ad4bbda6581614abd94b52c662
Bollsjen <55839056+Bollsjen@users.noreply.github.com>
2022-11-19 17:20:35 +0100

    Initial commit

---
+++ android/app/src/main/kotlin/dk/bollsjen/collect_words/MainActivity.kt
@@ -0,0 +1,6 @@
+package dk.bollsjen.collect_words
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/pages/HomePage.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/material.dart';
+import 'package:collect_words/components/collect_button.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<HomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<HomePage> {
+  final textController = TextEditingController();
+  String showText = "ss";
+  final _words = List.empty(growable: true);
+
+  @override
+  Widget build(BuildContext context){
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Collect Words'),
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Padding(
+            padding: EdgeInsets.all(5),
+            child: TextField(
+              decoration: InputDecoration(
+                  hintText: 'Type word here...'
+              ),
+              controller: textController,
+            ),
+          ),
+          CollectButton(title: 'SAVE WORD', color: Colors.blue[400], function: SaveWord),
+          CollectButton(title: 'CLEAR WORDS', color: Colors.blue[400], function: ClearWords),
+          CollectButton(title: 'SHOW WORDS', color: Colors.blue[400], function: ShowWords),
+          Text(
+            showText,
+          )
+        ],
+      ),
+    );
+  }
+
+  void SaveWord(){
+    _words.add(textController.text);
+  }
+
+  void ClearWords(){
+    _words.clear();
+  }
+
+  void ShowWords(){
+    setState(() {
+      showText = _words.toString();
+    });
+  }
+}
